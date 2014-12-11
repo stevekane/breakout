9701c934f492b7f3419b21b0835c216f83e28b30
Steve Kane <kanesteven@gmail.com>
2014-12-10 23:30:13 -0600

    more work sharpening api and simplifying input manager

--- src/InputManager.js
+++ src/InputManager.js
@@ -1,59 +1,62 @@
 module.exports = InputManager
 
-const STATE_LENGTH = 3
+const EVENT_SIZE   = 2
 const KEY_COUNT    = 256
-const QUEUE_LENGTH = 24
+const QUEUE_LENGTH = 10
+const KEYDOWN      = 0
+const JUSTDOWN     = 1
+const JUSTUP       = 2
 
 //[up/down, justDown, justUp]
-let isDown      = (states, keyCode) => states[keyCode*STATE_LENGTH]
-let justDown    = (states, keyCode) => states[keyCode*STATE_LENGTH+1]
-let justUp      = (states, keyCode) => states[keyCode*STATE_LENGTH+2]
-let setDown     = (states, keyCode, val) => states[keyCode*STATE_LENGTH] = val
-let setJustDown = (states, keyCode, val) => states[keyCode*STATE_LENGTH+1] = val
-let setJustUp   = (states, keyCode, val) => states[keyCode*STATE_LENGTH+2] = val
-let setState    = (states, keyCode, isDown, justDown, justUp) => {
-  states[keyCode*STATE_LENGTH]   = isDown
-  states[keyCode*STATE_LENGTH+1] = justDown
-  states[keyCode*STATE_LENGTH+2] = justUp
+let isDown  = (states, keyCode) => states[keyCode]
+let setDown = (states, keyCode, val) => states[keyCode] = val
+
+function EventQueue () {
+  let queue = new Int8Array(QUEUE_LENGTH * EVENT_SIZE)
+
+  queue.index = QUEUE_LENGTH - 1
+  return queue
+}
+
+//type is ENUM see above for defs
+function queueEvent (queue, keyCode, type) {
+  queue[queue.index*2]   = keyCode
+  queue[queue.index*2+1] = type
+  queue.index--
 }
 
 function InputManager (document) {
-  let downQueue     = []
-  let justDownQueue = []
-  let justUpQueue   = []
-  let states        = new Int8Array(KEY_COUNT * STATE_LENGTH)
+  let eventQueue = new EventQueue
+  let states     = new Int8Array(KEY_COUNT)
   
   let handleKeyDown = ({keyCode}) => {
-    let alreadyDown = isDown(states, keyCode)
-
+    if (!isDown(states, keyCode)) queueEvent(eventQueue, keyCode, JUSTDOWN)
     setDown(states, keyCode, 1)
-    setJustDown(states, keyCode, 1)
   }
 
   let handleKeyUp = ({keyCode}) => {
     setDown(states, keyCode, 0)
-    setJustUp(states, keyCode, 1)
+    queueEvent(eventQueue, keyCode, JUSTUP)
   }
 
-  this.states        = states
-  this.downQueue     = downQueue
-  this.justDownQueue = justDownQueue
-  this.justUpQueue   = justUpQueue
+  Object.defineProperty(this, "eventQueue", {
+    get () { return eventQueue } 
+  })
+
+  this.tick = (dT) => {
+    for (var i = 0, len = states.length; i < len; ++i) {
+      if (isDown(states, i)) queueEvent(eventQueue, i, KEYDOWN)
+    }    
+  }
+
+  this.flush = () => {
+    let i   = -1
+    let len = eventQueue.length
+
+    while (++i < len) eventQueue[i] = 0
+    eventQueue.index = QUEUE_LENGTH - 1
+  }
 
   document.addEventListener("keydown", handleKeyDown)
   document.addEventListener("keyup", handleKeyUp)
-}
-
-InputManager.prototype.tick = function (dT) {
-  this.downQueue     = []
-  this.justDownQueue = []
-  this.justUpQueue   = []
-
-  for (var i = 0, len = this.states.length; i < len; ++i) {
-    if (isDown(this.states, i))   this.downQueue.push(i)
-    if (justDown(this.states, i)) this.justDownQueue.push(i)
-    if (justUp(this.states, i))   this.justUpQueue.push(i)
-    setJustDown(this.states, i, 0)
-    setJustUp(this.states, i, 0)
-  }    
 }
--- src/ld.js
+++ src/ld.js
@@ -30,6 +30,8 @@ let game         = new Game(cache, loader, renderer, entityStore, sceneManager)
 function makeUpdate (game) {
   return function update () {
     im.tick()
+    //console.log(im.eventQueue)
+    im.flush()
     game.sceneManager.activeScene.update()
   }
 }
